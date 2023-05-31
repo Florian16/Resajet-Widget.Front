f5cf51c9e70b496970a5f21c13308d0a7a0cd7d7
Florian <florian.dem@hotmail.fr>
2023-05-31 22:01:10 +0200

    Configuration de quelques dossiers

--- resajet-widget/vite.config.ts
+++ resajet-widget/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import styleImport from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    styleImport({
+      libs: [
+        {
+          libraryName: 'tailwindcss',
+          resolveStyle: (name) => `tailwindcss/${name}`,
+        },
+      ],
+    }),],
 })
