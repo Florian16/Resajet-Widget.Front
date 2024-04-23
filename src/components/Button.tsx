b59f560bfa7f8196541b3069c6686a0fc7531eb6
Florian16 <86965438+Florian16@users.noreply.github.com>
2024-04-23 23:01:26 +0200

    Mise en place du logo

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,5 @@
 import { CompanyContextProps } from "../contexts/CompanyContext";
+import logo from "../assets/logo/logo.png";
 
 type ButtonProps = {
   companyContext: CompanyContextProps;
@@ -15,6 +16,8 @@ export default function Button({
   isOpen,
   color,
 }: ButtonProps) {
+  console.log(color);
+
   return (
     <div
       className={`resajet-button`}
@@ -34,15 +37,38 @@ export default function Button({
             : "not-open"
         }`}
       >
-        <svg
-          style={{ marginTop: "6px" }}
-          width="32"
-          height="32"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <line x1="8" y1="8" x2="24" y2="24" stroke={color} strokeWidth="2" />
-          <line x1="8" y1="24" x2="24" y2="8" stroke={color} strokeWidth="2" />
-        </svg>
+        {!isOpen && !isToggleOpen ? (
+          <img
+            src={logo}
+            className={`resajet-button-logo  ${
+              color === "white" ? "" : "resajet-button-logo-black"
+            } `}
+          />
+        ) : (
+          <svg
+            style={{ marginTop: "6px" }}
+            width="32"
+            height="32"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line
+              x1="8"
+              y1="8"
+              x2="24"
+              y2="24"
+              stroke={color}
+              strokeWidth="2"
+            />
+            <line
+              x1="8"
+              y1="24"
+              x2="24"
+              y2="8"
+              stroke={color}
+              strokeWidth="2"
+            />
+          </svg>
+        )}
       </span>
     </div>
   );
